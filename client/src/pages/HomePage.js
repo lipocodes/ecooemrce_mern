6f176fb32c8eb8e976eae37bb68610f39f0b25fb
lipocodes <lipocodes@yahoo.com>
2023-04-21 11:22:09 +0300

    210423_1125

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -167,7 +167,7 @@ useEffect(()=>{
           <div className='d-flex flex-wrap'>
             {products?.map((p)=> (
                  <div  key={p._id} className="card m-2" style={{width: '18rem', }} >
-                 <img className="card-img-top" src={`/api/v1/product/product-photo/${p._id}`} alt={p.name} />
+                 <img className="card-img-top" src={`${process.env.REACT_APP_API}/api/v1/product/product-photo/${p._id}`} alt={p.name} />
                  <div className="card-body">
                    <h5 className="card-title">{p.name}</h5>
                    <p className="card-text">{p.description.substring(0,30)}...</p>
